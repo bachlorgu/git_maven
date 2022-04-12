b75c34ae24fdd720979a2ecbb42c1fc97cb04315
gugu <1345716494@qq.com>
2022-04-12 17:54:09 +0800

    hebing

--- src/main/java/gugu/Test.java
+++ src/main/java/gugu/Test.java
@@ -2,6 +2,6 @@ package gugu;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello git gu1");
+        System.out.println("hello git master");
     }
 }
